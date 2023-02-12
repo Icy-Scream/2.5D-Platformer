7e78e90f6e924a1615d1b4ea06693006b6c48aae
Icy-Scream <jdpetta21@gmail.com>
2023-02-12 13:10:37 -1000

    Adjusted the Jump Mechanics

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,3 +1,4 @@
+using JetBrains.Annotations;
 using System.Net;
 using Unity.VisualScripting;
 using UnityEngine;
@@ -9,8 +10,9 @@ public class Player : MonoBehaviour
     [SerializeField] private float _speed = 2.0f;
     [SerializeField] private float _jumpStrength = 15.0f;
     [SerializeField]private float _gravity = 1.0f;
+    private bool _doubleJump;
     private float _yVelocity;
-
+    [SerializeField] private bool _groundPlayer;
     private void Awake()
     {
         _gameInput = GetComponent<GameInput>();
@@ -19,22 +21,28 @@ public class Player : MonoBehaviour
 
     private void Update()
     {
-        Vector3 _direction = _gameInput.GetMovementVectorNormalized();
-        Vector3 _velocity = _direction * _speed;
-        
+
+        _groundPlayer = _controller.isGrounded;
         Debug.Log(_yVelocity);
 
-        if (_controller.isGrounded == true && _gameInput.IsJumping()) 
-        {
-            _yVelocity = _jumpStrength;
-        }  
-        else
-        {
-            _yVelocity -= _gravity;
+        if (_groundPlayer == true) { 
+            _controller.Move(Vector3.zero);
+             _yVelocity = -_gravity;
         }
+
+        if (_groundPlayer == true && _gameInput.IsJumping()) {
+            _yVelocity += _jumpStrength;
+            _doubleJump = false;
+        }
+        else if(_groundPlayer == false)
+        _yVelocity -= _gravity * Time.deltaTime;
        
-        _velocity.y = _yVelocity;
-        _controller.Move(_velocity * Time.deltaTime);
+          
+
+        Vector3 _direction = _gameInput.GetMovementVectorNormalized();
+        Vector3 _velocity = _direction * _speed;
+        Vector3 movement = new Vector3(_velocity.x, _yVelocity, 0);
+        _controller.Move(movement * Time.deltaTime);
 
     }
 
