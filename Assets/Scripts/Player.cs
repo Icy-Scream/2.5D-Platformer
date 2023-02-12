4b7b635fa0c2f1d01714f5a031e4b2e37b4f14bc
Icy-Scream <jdpetta21@gmail.com>
2023-02-12 13:33:18 -1000

    added double jump

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -11,6 +11,7 @@ public class Player : MonoBehaviour
     [SerializeField] private float _jumpStrength = 15.0f;
     [SerializeField]private float _gravity = 1.0f;
     private bool _doubleJump;
+    private float _jumpDelay;
     private float _yVelocity;
     [SerializeField] private bool _groundPlayer;
     private void Awake()
@@ -23,7 +24,7 @@ public class Player : MonoBehaviour
     {
 
         _groundPlayer = _controller.isGrounded;
-        Debug.Log(_yVelocity);
+
 
         if (_groundPlayer == true) { 
             _controller.Move(Vector3.zero);
@@ -33,15 +34,33 @@ public class Player : MonoBehaviour
         if (_groundPlayer == true && _gameInput.IsJumping()) {
             _yVelocity += _jumpStrength;
             _doubleJump = false;
+            _jumpDelay = Time.time + 0.3f;
+            
         }
-        else if(_groundPlayer == false)
-        _yVelocity -= _gravity * Time.deltaTime;
-       
-          
+        else if(_groundPlayer == false) 
+        {
+            if (!_doubleJump && _gameInput.IsJumping() && Time.time > _jumpDelay) 
+            {
+                _doubleJump = true;
+                if(_yVelocity < 0) { 
+                    _yVelocity = 0;
+                    _yVelocity += 5f;
+                }
+                else
+                    _yVelocity += 5f;
 
+            }
+
+        _yVelocity -= _gravity * Time.deltaTime;
+
+        }
+
+        var _yMaxVelocity = Mathf.Clamp(_yVelocity, -20, 100f);
+        
         Vector3 _direction = _gameInput.GetMovementVectorNormalized();
         Vector3 _velocity = _direction * _speed;
-        Vector3 movement = new Vector3(_velocity.x, _yVelocity, 0);
+        
+        Vector3 movement = new Vector3(_velocity.x, _yMaxVelocity, 0);
         _controller.Move(movement * Time.deltaTime);
 
     }
