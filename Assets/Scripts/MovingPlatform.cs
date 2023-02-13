0a66eea43c3f7c672c6828e259c5343c1aa1af2f
Icy-Scream <jdpetta21@gmail.com>
2023-02-13 12:18:15 -1000

    Moving Platforms with Waypoints

--- Assets/Scripts/MovingPlatform.cs
+++ Assets/Scripts/MovingPlatform.cs
@@ -5,24 +5,44 @@ using UnityEngine;
 public class MovingPlatform : MonoBehaviour
 {
     [SerializeField] private float _speed;
+    [SerializeField] private Transform[] _wayPoints;
     private bool _returnPosition;
 
     // Update is called once per frame
     void Update()
     {
-        if(transform.position.x < 32f && !_returnPosition) 
+        if (!_returnPosition) 
         { 
-            transform.Translate(Vector3.right * _speed * Time.deltaTime);
-            if(transform.position.x > 30f)
-            _returnPosition = true;
+            transform.position = Vector3.MoveTowards(transform.position, _wayPoints[0].position, _speed * Time.deltaTime);
+            if (transform.position == _wayPoints[0].position)
+                _returnPosition = true;
+            
         }
-        
-        else if (transform.position.x > 19.7f && _returnPosition) 
+       else if (_returnPosition) 
         { 
-          transform.Translate(Vector3.left * _speed * Time.deltaTime);
+           transform.position = Vector3.MoveTowards(transform.position, _wayPoints[1].position, _speed * Time.deltaTime);
+            if (transform.position == _wayPoints[1].position)
+                _returnPosition = false;
+
+        }
+    }
+
+    private void StaticPositionMovement()
+    {
+        if (transform.position.x < 32f && !_returnPosition)
+        {
+            transform.Translate(Vector3.right * _speed * Time.deltaTime);
+            if (transform.position.x > 30f)
+                _returnPosition = true;
+        }
+
+        else if (transform.position.x > 19.7f && _returnPosition)
+        {
+            transform.Translate(Vector3.left * _speed * Time.deltaTime);
             if (transform.position.x < 19.8f)
                 _returnPosition = false;
 
         }
     }
+
 }
